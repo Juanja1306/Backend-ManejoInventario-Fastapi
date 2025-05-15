a845e8c3ca5a49ac749791fdd5272538802490ce
Juanja1306 <juanjamalovega@gmail.com>
2025-05-15 13:02:44 -0500

    Create settings.py
    maneja la Carga del .env

---
+++ app/settings.py
@@ -0,0 +1,16 @@
+# app/settings.py
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DB_USER:     str
+    DB_PASSWORD: str
+    DB_SERVER:   str
+    DB_PORT:     int
+    DB_NAME:     str
+    DRIVER:      str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
