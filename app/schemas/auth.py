4a04aea4d2d5d0a2a2915f88495c3d8a84483175
Juanja1306 <juanjamalovega@gmail.com>
2025-05-26 10:33:51 -0500

    Update auth.py

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -2,11 +2,7 @@
 from pydantic import BaseModel
 from typing import Dict, List
 
-class TokenData(BaseModel):
-    idUsuario: int
-    correo: str
-    roles: Dict[str, str]  # {aplicacion: rol}
+class AuthResponseWithMeta(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
 
-class Credentials(BaseModel):
-    correo: str
-    contrasenia: str
