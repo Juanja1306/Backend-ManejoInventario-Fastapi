125999e8f77c2d27a9a26796b1b44446f2320924
Juanja1306 <juanjamalovega@gmail.com>
2025-05-26 10:33:13 -0500

    Reestructuracion

--- app/schemas/categorias.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, ConfigDict
-
-class CategoriaSchema(BaseModel):
-    idCategoria: str
-    nombreCategoria: str
-
-    model_config = ConfigDict(from_attributes=True)
