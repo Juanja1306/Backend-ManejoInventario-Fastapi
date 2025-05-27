3bf721074be719e253e58bfc50c07b5bb0d06ef2
Juanja1306 <juanjamalovega@gmail.com>
2025-05-27 09:42:09 -0500

    Update tecnico.py

--- app/routers/tecnico.py
+++ app/routers/tecnico.py
@@ -1,15 +1,16 @@
+#tecnico.py
+
 from fastapi import APIRouter
 from app.utils import require_role
-from app.schemas.metadata import MetadataRead
 
-router = APIRouter(prefix="/tecnico", tags=["metadata"], dependencies=[require_role(["BODEGA"])])
+router = APIRouter(prefix="/tecnico", tags=["Tecnico"], dependencies=[require_role(["tecnico"])])
 
 @router.post(
     "/", 
     # response_model=MetadataRead, 
     status_code=201)
 
-def create_metadata():
+def create_solicitud():
     print("Creating metadata...")
 #     return []
 
