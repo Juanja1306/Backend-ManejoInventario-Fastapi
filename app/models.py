3937fde133ff12da131a599f03f93e4425abc19b
Juanja1306 <juanjamalovega@gmail.com>
2025-05-28 08:48:05 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -158,7 +158,7 @@ class Peticion(Base):
     idPeticion: Mapped[int] = mapped_column(
         Integer,
         primary_key=True,
-        autoincrement=False
+        autoincrement=True
     )
     solicitante: Mapped[str] = mapped_column(
         String(100),
@@ -208,7 +208,7 @@ class ProductoPeticion(Base):
     idPeticionProducto: Mapped[int] = mapped_column(
         Integer,
         primary_key=True,
-        autoincrement=False
+        autoincrement=True
     )
     idPeticion: Mapped[int] = mapped_column(
         Integer,
